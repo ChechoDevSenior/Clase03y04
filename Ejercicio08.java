80367f6bd395b71418091d6d9d95633beb372dcc
Sergio Arturo Pe?a Chaparro <sergioarturo9306@gmail.com>
2024-11-22 23:16:52 -0500

    Ejercicio08

--- Ejercicio08.java
+++ Ejercicio08.java
@@ -1,5 +1,28 @@
 package Clase03_04;
+import java.util.Scanner;
 
 public class Ejercicio08 {
-    
+    public static void main(String[] args) {
+        var scanner = new Scanner (System.in);
+        System.out.print("Ingrese su edad: ");
+        var edad = scanner.nextInt();
+
+        if (edad<0) {
+            System.out.println("Es una edad invalida.");
+        } else if (edad <= 5) {
+            System.out.println("Pertenece a la primera infancia.");
+        } else if (edad <=11) {
+            System.out.println("Pertenece a la infancia.");
+        } else if (edad <=18) {
+            System.out.println("Pertenece a la adolescencia.");
+        } else if (edad <=26) {
+            System.out.println("Pertenece a la juventd.");
+        } else {if (edad <=59) {
+            System.out.println("Pertenece a la adultez.");
+            } else {
+                System.out.println("Es persona mayor.");
+                   }
+        }    
+        scanner.close(); 
+    }
 }
